83ad3af51f944948cb726cbbdd8d02043c585b0c
k2tzumi <katzumi+github@gmail.com>
2024-01-28 17:15:30 +0900

    Append header pattern

--- tests/Feature/CollectCodeCoverageTest.php
+++ tests/Feature/CollectCodeCoverageTest.php
@@ -40,6 +40,45 @@ class CollectCodeCoverageTest extends TestCase
     }
 
     /**
+     * @test
+     * @dataProvider headerPattern
+     * @covers ::handle
+     * @return void
+     */
+    public function testHandleWitHeader(array $server): void
+    {
+        setup:
+        $instance = $this->createInstance();
+
+        // Create requests and closures
+        $request = Request::create('/test', 'GET', server: $server);
+        $next = function ($request) {
+            return new Response('OK');
+        };
+
+        when:
+        $response = $instance->handle($request, $next);
+
+        then:
+        $this->assertEquals('OK', $response->getContent());
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+
+    /**
+     * @return iterable
+     */
+    public static function headerPattern(): iterable
+    {
+        return [
+            'Normal' => [['X-Runn-Trace' => '{ "id": "a1b7b02?step=1" }']],
+            'Other hedername' => [['X-Request-Id' => '{ "id": "a1b7b02?step=1" }']],
+            'Empty value' => [['X-Runn-Trace' => '']],
+            'Non json value' => [['X-Runn-Trace' => 'non-json']],
+        ];
+    }
+
+
+    /**
      * @return CollectCodeCoverage
      */
     private function createInstance() : CollectCodeCoverage {
