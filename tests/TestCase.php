f7a7ec12c1386c0cd71358b0cd2d2b22146f0aa7
k2tzumi <katzumi+github@gmail.com>
2024-01-28 17:21:12 +0900

    Define config

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -14,4 +14,18 @@ abstract class TestCase extends BaseTestCase
     {
         return [];
     }
+
+    /**
+     * Define environment setup.
+     *
+     * @param \Illuminate\Foundation\Application $app
+     * @return void
+     */
+    protected function getEnvironmentSetUp($app): void
+    {
+        $app['config']->set('coverage', [
+            'phpunit_config_path' =>  'phpunit.xml',
+            'runn-trace-header' => 'X-Runn-Trace',
+        ]);
+    }
 }
